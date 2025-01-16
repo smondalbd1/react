4ed57a45c0157c48919122c6f9c08a23a6408701
Sumon Mondal <smondal.bd1@gmail.com>
2025-01-16 21:04:12 +0600

    New condition added for empty value

--- tapaScriptCars/src/SingleCar.jsx
+++ tapaScriptCars/src/SingleCar.jsx
@@ -2,21 +2,29 @@ import PropTypes from "prop-types";
 
 function SingleCar({ car }) {
   return (
-    <div className="border p-4 rounded-lg shadow-sm">
-      <h2 className="text-xl font-bold ">{car.title}</h2>
-      <p className="text-black ">
-        <strong>Brand:</strong> {car.brand}
-      </p>
-      <p className="text-black">
-        <strong>Year:</strong> {car.year}
-      </p>
-      <p className="text-black">
-        {" "}
-        <strong>Price:</strong> {car.price}
-      </p>
-      <p className="text-black">
-        <strong>Premium:</strong> {car.isPremium ? "Yes" : "No"}
-      </p>
+    <div key={car.id} className="border p-4 rounded-lg shadow-sm">
+      {car.title && <h2 className="text-xl font-bold ">{car.title}</h2>}
+
+      {car.brand && (
+        <p className="text-black ">
+          <strong>Brand:</strong> {car.brand}
+        </p>
+      )}
+      {car.year && (
+        <p className="text-black">
+          <strong>Year:</strong> {car.year}
+        </p>
+      )}
+      {car.price && (
+        <p className="text-black">
+          <strong>Price:</strong> {car.price}
+        </p>
+      )}
+      {car.isPremium && (
+        <p className="text-black">
+          <strong>Premium:</strong> {car.isPremium ? "Yes" : "No"}
+        </p>
+      )}
     </div>
   );
 }
